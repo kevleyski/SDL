b1c3a0d3273d122faf7a58a72ee97df8f118aab6
Kevin Staunton-Lambert <kevin.staunton-lambert@metacdn.com>
2024-11-26 12:44:56 +1100

    KJSL building workaround for SDL2 on Intel

--- kjsl_build_macos.sh
+++ kjsl_build_macos.sh
@@ -1,5 +1,9 @@
-mkdir cmake-build-debug
-cd cmake-build-debug
-cmake .. "-DCMAKE_OSX_ARCHITECTURES=arm64;x86_64"
-cmake --build .
+#!/bin/bash
+#rm -rf cmake-build-debug
+if [ ! -d cmake-build-debug ]; then
+  mkdir cmake-build-debug
+  cd cmake-build-debug
+  cmake .. "-DCMAKE_OSX_ARCHITECTURES=arm64;x86_64"
+fi
+cmake --build . -j $(nproc)
 sudo cmake --install .
